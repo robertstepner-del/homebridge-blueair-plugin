712b04fb2709d0722998da5225113a54e7583239
robertstepner-del <robert.stepner@icloud.com>
2026-02-05 01:49:00 +0000

    fix: enhance UI initialization and error handling for BlueAir plugin

--- homebridge-ui/server.js
+++ homebridge-ui/server.js
@@ -11,16 +11,8 @@ var _ = require('lodash');
 class Logger {
   _debug;
   _Reset = '\x1b[0m';
-  _Bright = '\x1b[1m';
-  _Dim = '\x1b[2m';
-
-  _FgBlack = '\x1b[30m';
   _FgRed = '\x1b[31m';
-  _FgGreen = '\x1b[32m';
   _FgYellow = '\x1b[33m';
-  _FgBlue = '\x1b[34m';
-  _FgMagenta = '\x1b[35m';
-  _FgCyan = '\x1b[36m';
   _FgWhite = '\x1b[37m';
   _FgGray = '\x1b[90m';
 
@@ -56,29 +48,15 @@ class Logger {
  * Main server-side script called when Custom UI client sends requests
  */
 class UiServer extends HomebridgePluginUiServer {
-
   logger;
-  config;
   api;
 
   constructor() {
     super();
-    // Obtain the plugin configuration from homebridge config JSON file.
-    const config = require(this.homebridgeConfigPath).platforms.find((obj) => obj.platform === 'blueair-purifier');
-    this.logger = new Logger(config?.uiDebug ? config.uiDebug : false);
-    this.logger.info('Custom UI created.');
-    this.logger.debug(`ENV:\n${JSON.stringify(process.env, null, 2)}`);
-
-    this.onRequest('/mergeToDefault', async ({ config }) => {
-      _.defaultsDeep(config, defaultConfig);
-      config.devices.forEach((device) => {
-        _.defaultsDeep(device, defaultDeviceConfig);
-      });
-      this.config = config;
-      this.logger.setDebugEnabled(config.uiDebug ? config.uiDebug : false);
-      this.logger.debug(`Merged config:\n${JSON.stringify(config, null, 2)}`);
-      return config;
-    });
+    
+    // Initialize logger - don't try to read config here as it may not exist
+    this.logger = new Logger(false);
+    this.logger.info('BlueAir Custom UI server started.');
 
     this.onRequest('/getDefaults', async () => {
       return {
@@ -87,36 +65,65 @@ class UiServer extends HomebridgePluginUiServer {
       };
     });
 
+    this.onRequest('/mergeToDefault', async ({ config }) => {
+      try {
+        if (!config || typeof config !== 'object') {
+          config = {};
+        }
+        _.defaultsDeep(config, defaultConfig);
+        if (config.devices && Array.isArray(config.devices)) {
+          config.devices.forEach((device) => {
+            _.defaultsDeep(device, defaultDeviceConfig);
+          });
+        } else {
+          config.devices = [];
+        }
+        this.logger.setDebugEnabled(config.uiDebug || false);
+        return config;
+      } catch (e) {
+        this.logger.error(`mergeToDefault error: ${e}`);
+        return { ...defaultConfig, devices: [] };
+      }
+    });
+
     this.onRequest('/discover', async ({ username, password, region }) => {
       try {
+        if (!username || !password) {
+          throw new Error('Username and password are required');
+        }
         this.api = new BlueAirAwsApi(username, password, region, this.logger);
         await this.api.login();
         const devices = await this.api.getDevices();
-
-        return devices;
+        return devices || [];
       } catch (e) {
-        const msg = e instanceof Error ? e.stack : e;
-        this.logger.error(`Device discovery failed:\n${msg}`);
-        throw new RequestError(`Device discovery failed:\n${msg}`);
+        const msg = e instanceof Error ? e.message : String(e);
+        this.logger.error(`Device discovery failed: ${msg}`);
+        throw new RequestError(`Discovery failed: ${msg}`);
       }
     });
 
     this.onRequest('/getInitialDeviceStates', async ({ accountUuid, uuids }) => {
       try {
+        if (!this.api) {
+          throw new Error('Not logged in. Please discover devices first.');
+        }
+        if (!accountUuid || !uuids || !uuids.length) {
+          throw new Error('Missing accountUuid or device uuids');
+        }
         return await this.api.getDeviceStatus(accountUuid, uuids);
       } catch (e) {
-        const msg = e instanceof Error ? e.stack : e;
-        this.logger.error(`Failed to get initial device states:\n${msg}`);
-        throw new RequestError(`Failed to get initial device states:\n${msg}`);
+        const msg = e instanceof Error ? e.message : String(e);
+        this.logger.error(`Failed to get device states: ${msg}`);
+        throw new RequestError(`Failed to get device states: ${msg}`);
       }
     });
 
-    // inform client-side script that we are ready to receive requests.
+    // Signal that the server is ready to receive requests
     this.ready();
   }
 }
 
-// start the instance of the class
+// Start the server
 (() => {
   return new UiServer();
 })();
